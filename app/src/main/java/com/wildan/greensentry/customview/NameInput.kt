218d6fa0f6d48cd7c6d29038a2495c878bde083c
DarkCount16 <wildanbillah2003@gmail.com>
2024-06-05 17:26:34 +0700

    (nambah)

---
+++ app/src/main/java/com/wildan/greensentry/customview/NameInput.kt
@@ -0,0 +1,4 @@
+package com.wildan.greensentry.customview
+
+class NameInput {
+}
---
+++ app/src/main/java/com/wildan/greensentry/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.wildan.greensentry.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wildan.greensentry.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
